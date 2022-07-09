ec5aec77d5fae4e95bd4a801bbd798ae7779d0f8
vitaliiloboda <vitalii.loboda@gmail.com>
2022-07-09 17:50:31 +0300

    issue_15 tests

--- group_883/mainapp/tests.py
+++ group_883/mainapp/tests.py
@@ -1,22 +1,32 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.urls import reverse
+
 from mainapp.models import Category, Article, Tag, Comment
 from personal_account.models import User
 
 
 class TestMainappSmoke(TestCase):
     status_ok = 200
+    status_redirect = 302
+    username = 'django'
+    password = 'django'
 
     def setUp(self) -> None:
         self.category = Category.objects.create(
             title='Разработка'
         )
+        for i in range(10):
+            Category.objects.create(
+                title=f'Title {i}'
+            )
         self.tag = Tag.objects.create(
             title='Python'
         )
         self.user = User.objects.create(
             description='description',
-            username='username',
+            username='dm',
+            password='Freedayz',
             first_name='first name',
             last_name='last name'
         )
@@ -36,11 +46,19 @@ class TestMainappSmoke(TestCase):
                 title=f'title {i}',
                 short_desc=f'short description {i}',
                 body=f'body {i}',
+                moderated=1
             )
         self.comment = Comment.objects.create(
             article=self.article,
             user=self.user,
             body='body'
+        )
+        self.superuser = User.objects.create_superuser(
+            description='super description',
+            username=self.username,
+            password=self.password,
+            first_name='super first name',
+            last_name='super last name'
         )
         self.client = Client()
 
@@ -52,8 +70,24 @@ class TestMainappSmoke(TestCase):
         response = self.client.get('/help/')
         self.assertEqual(response.status_code, self.status_ok)
 
-    # def test_article_url(self):
-    #     pass
+    def test_article_url(self):
+
+        for article in Article.objects.all():
+            response = self.client.get(reverse('mainapp:article', kwargs={'pk': article.pk}))
+            self.assertEqual(response.status_code, self.status_ok)
+
+    def test_category_url(self):
+        for category in Category.objects.all():
+            response = self.client.get(reverse('mainapp:category', kwargs={'pk': category.pk}))
+            self.assertEqual(response.status_code, self.status_ok)
+
+    # def test_comment_create(self):
+    #     response = self.client.get(reverse('mainapp:article', kwargs={'pk': self.article.pk}))
+    #     self.assertEqual(response.status_code, self.status_ok)
+    #     self.assertTrue(response.context['user'].is_anonymous)
     #
+    #     self.client.login(username=self.username, password=self.password)
+    #     self.assertFalse(response.context['user'].is_anonymous)
+
     # def tearDown(self) -> None:
     #     pass
